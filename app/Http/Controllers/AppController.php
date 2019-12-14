3e79108640aa5fe37f6072ebd04a7b612a680c3b
heni2424 <henimarly8@gmail.com>
2019-12-14 13:12:21 +0300

    Add project dependaecy

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\app;
+use Illuminate\Http\Request;
+
+class AppController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\app  $app
+     * @return \Illuminate\Http\Response
+     */
+    public function show(app $app)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\app  $app
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(app $app)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\app  $app
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, app $app)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\app  $app
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(app $app)
+    {
+        //
+    }
+}
